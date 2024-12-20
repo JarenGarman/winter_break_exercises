9c6e36a43f1e7e577c402fd020560c335eaba7c2
Jaren Garman <jarengarman@protonmail.com>
2024-12-20 15:26:41 -0500

    test: Formatting car_spec

--- car_dealership/spec/car_spec.rb
+++ car_dealership/spec/car_spec.rb
@@ -1,40 +1,31 @@
-require 'rspec'
-require './lib/car'
+# frozen_string_literal: true
+
+require_relative 'spec_helper'
 
 RSpec.describe Car do
-  it 'exists' do
-    car = Car.new("Ford Mustang", 1500, 36)
+  subject(:car) { described_class.new('Ford Mustang', 1500, 36) }
 
-    expect(car).to be_a Car
-  end
+  describe '#initialize' do
+    it { is_expected.to be_an_instance_of(described_class) }
 
-  it 'has a make' do
-    car = Car.new("Ford Mustang", 1500, 36)
+    it 'has a make' do
+      expect(car.make).to eq('Ford')
+    end
 
-    expect(car.make).to eq("Ford")
-  end
+    it 'has a model' do
+      expect(car.model).to eq('Mustang')
+    end
 
-  it 'has a model' do
-    car = Car.new("Ford Mustang", 1500, 36)
+    it 'has a monthly payment' do
+      expect(car.monthly_payment).to eq(1500)
+    end
 
-    expect(car.model).to eq("Mustang")
-  end
+    it 'has a loan length' do
+      expect(car.loan_length).to eq(36)
+    end
 
-  it 'has a monthly payment' do
-    car = Car.new("Ford Mustang", 1500, 36)
-
-    expect(car.monthly_payment).to eq(1500)
-  end
-
-  it 'has a loan length' do
-    car = Car.new("Ford Mustang", 1500, 36)
-
-    expect(car.loan_length).to eq(36)
-  end
-
-  it 'has a total cost' do
-    car = Car.new("Ford Mustang", 1500, 36)
-
-    expect(car.total_cost).to eq(54000)
+    it 'has a total cost' do
+      expect(car.total_cost).to eq(54_000)
+    end
   end
 end
