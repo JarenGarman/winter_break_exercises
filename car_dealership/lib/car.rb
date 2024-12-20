80c317608496d3217e15567c74ecb3d7306e02f3
Jaren Garman <jarengarman@protonmail.com>
2024-12-20 15:31:47 -0500

    feature: Add paint! and color methods

--- car_dealership/lib/car.rb
+++ car_dealership/lib/car.rb
@@ -2,12 +2,16 @@
 
 # TLD
 class Car
-  attr_reader :make, :model, :monthly_payment, :loan_length, :total_cost
+  attr_reader :make, :model, :monthly_payment, :loan_length, :total_cost, :color
 
   def initialize(make_and_model, monthly_payment, loan_length)
     @make, @model = make_and_model.split
     @monthly_payment = monthly_payment
     @loan_length = loan_length
     @total_cost = monthly_payment * loan_length
+  end
+
+  def paint!(color)
+    @color = color
   end
 end
