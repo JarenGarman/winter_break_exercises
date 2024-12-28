01995558942a6edaf0a940307a23b95bcb3863c7
Jaren Garman <jarengarman@protonmail.com>
2024-12-27 22:20:30 -0500

    feat: Add write method

--- public_library/lib/author.rb
+++ public_library/lib/author.rb
@@ -10,4 +10,13 @@ class Author
     @name = [author_details[:first_name], author_details[:last_name]].join(' ')
     @books = []
   end
+
+  def write(title, publication_date)
+    Book.new({
+               author_first_name: @name.split[0],
+               author_last_name: @name.split[1],
+               title: title,
+               publication_date: publication_date
+             })
+  end
 end
