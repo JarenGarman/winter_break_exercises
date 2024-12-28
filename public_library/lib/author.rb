b42a2fb9656d1fd6ae6a1c33bab10a55ba54f221
Jaren Garman <jarengarman@protonmail.com>
2024-12-27 22:15:30 -0500

    feat: Add books attribute

--- public_library/lib/author.rb
+++ public_library/lib/author.rb
@@ -4,9 +4,10 @@ require_relative 'book'
 
 # TLD
 class Author
-  attr_reader :name
+  attr_reader :name, :books
 
   def initialize(author_details)
     @name = [author_details[:first_name], author_details[:last_name]].join(' ')
+    @books = []
   end
 end
