bb1963d05e17e0395233ec3fdbc50913bf3becba
Jaren Garman <jarengarman@protonmail.com>
2024-12-27 22:05:30 -0500

    test: Add test for publication_year

--- public_library/spec/book_spec.rb
+++ public_library/spec/book_spec.rb
@@ -20,5 +20,9 @@ RSpec.describe Book do
     it 'has an author' do
       expect(book.author).to eq('Harper Lee')
     end
+
+    it 'has a publication year' do
+      expect(book.publication_year).to eq('1960')
+    end
   end
 end
