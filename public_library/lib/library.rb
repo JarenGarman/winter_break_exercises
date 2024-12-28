3528dae276aa8565a30693ccfadafea6830f15f2
Jaren Garman <jarengarman@protonmail.com>
2024-12-27 22:44:00 -0500

    feat: Add authors attribute and books method

--- public_library/lib/library.rb
+++ public_library/lib/library.rb
@@ -2,9 +2,14 @@
 
 # TLD
 class Library
-  attr_reader :name
+  attr_reader :name, :authors
 
   def initialize(name)
     @name = name
+    @authors = []
+  end
+
+  def books
+    @authors.map(&:books).flatten
   end
 end
