12be2e2d5a5997d30cccc7a2b455663b1df1670c
Jaren Garman <jarengarman@protonmail.com>
2024-12-27 22:06:35 -0500

    feat: Add publication_year attribute

--- public_library/lib/book.rb
+++ public_library/lib/book.rb
@@ -2,10 +2,11 @@
 
 # TLD
 class Book
-  attr_reader :title, :author
+  attr_reader :title, :author, :publication_year
 
   def initialize(book_details)
     @title = book_details[:title]
     @author = [book_details[:author_first_name], book_details[:author_last_name]].join(' ')
+    @publication_year = book_details[:publication_date][-4..]
   end
 end
