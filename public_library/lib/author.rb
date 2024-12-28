8ce7ad420cd9ccbe0dd8c746df47c5eef451b827
Jaren Garman <jarengarman@protonmail.com>
2024-12-27 22:24:46 -0500

    feat: Add book to books

--- public_library/lib/author.rb
+++ public_library/lib/author.rb
@@ -12,11 +12,13 @@ class Author
   end
 
   def write(title, publication_date)
-    Book.new({
-               author_first_name: @name.split[0],
-               author_last_name: @name.split[1],
-               title: title,
-               publication_date: publication_date
-             })
+    book = Book.new({
+                      author_first_name: @name.split[0],
+                      author_last_name: @name.split[1],
+                      title: title,
+                      publication_date: publication_date
+                    })
+    @books << book
+    book
   end
 end
