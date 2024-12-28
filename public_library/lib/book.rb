9f1b45f41184906785164562579747bb6ab9dd9f
Jaren Garman <jarengarman@protonmail.com>
2024-12-28 00:10:00 -0500

    feat: Add checkout and checked_out_amount methods

--- public_library/lib/book.rb
+++ public_library/lib/book.rb
@@ -2,11 +2,16 @@
 
 # TLD
 class Book
-  attr_reader :title, :author, :publication_year
+  attr_reader :title, :author, :publication_year, :checked_out_amount
 
   def initialize(book_details)
     @title = book_details[:title]
     @author = [book_details[:author_first_name], book_details[:author_last_name]].join(' ')
     @publication_year = book_details[:publication_date][-4..]
+    @checked_out_amount = 0
+  end
+
+  def checkout
+    @checked_out_amount += 1
   end
 end
