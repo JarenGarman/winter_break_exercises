3b71b8db02de940263cde706686cc6c3d49f24a4
Jaren Garman <jarengarman@protonmail.com>
2024-12-30 21:04:12 -0500

    test: Fix decryption test

--- enigma/spec/enigma_spec.rb
+++ enigma/spec/enigma_spec.rb
@@ -20,10 +20,12 @@ RSpec.describe Enigma do
   end
 
   describe '#decrypt' do
-    expect(enigma.decrypt('keder ohulw', '02715', '040895')).to eq({
-                                                                     decryption: 'hello world',
-                                                                     key: '02715',
-                                                                     date: '040895'
-                                                                   })
+    it 'can decrypt a message' do
+      expect(enigma.decrypt('keder ohulw', '02715', '040895')).to eq({
+                                                                       decryption: 'hello world',
+                                                                       key: '02715',
+                                                                       date: '040895'
+                                                                     })
+    end
   end
 end
