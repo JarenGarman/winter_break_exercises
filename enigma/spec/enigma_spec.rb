7b54a9664b12fb80bc1f5f917816cce8bd608acd
Jaren Garman <jarengarman@protonmail.com>
2025-01-01 18:50:29 -0500

    test: Add tests for crack method

--- enigma/spec/enigma_spec.rb
+++ enigma/spec/enigma_spec.rb
@@ -58,4 +58,20 @@ RSpec.describe Enigma do
       expect(enigma.decrypt(todays_crypt[:encryption], '02715')[:decryption]).to eq('hello world')
     end
   end
+
+  describe '#crack' do
+    let(:todays_date) { enigma.encrypt('hello world end') }
+
+    it 'can crack message using given date' do
+      expect(enigma.crack('vjqtbeaweqihssi', '291018')).to eq({
+                                                                decryption: 'hello world end',
+                                                                date: '291018',
+                                                                key: '08304'
+                                                              })
+    end
+
+    it 'can crack message using todays date' do
+      expect(enigma.crack(todays_date[:encryption])[:decryption]).to eq('hello world end')
+    end
+  end
 end
