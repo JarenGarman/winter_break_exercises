43870f524c5b5ea8dcd632221abc4cf9af7fef25
Jaren Garman <jarengarman@protonmail.com>
2025-01-01 18:32:59 -0500

    feat: Complete encrypt runner

--- enigma/lib/encrypt.rb
+++ enigma/lib/encrypt.rb
@@ -1,3 +1,9 @@
 # frozen_string_literal: true
 
 require_relative 'enigma'
+
+message_file, filename = ARGV
+message = File.read(message_file)
+encrypted = Enigma.new.encrypt(message)
+File.write(filename, encrypted[:encryption])
+puts "Created '#{filename}' with the key #{encrypted[:key]} and date #{encrypted[:date]}"
