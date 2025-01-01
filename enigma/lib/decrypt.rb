ebbe13eae8cd3b01548b99d80c1f1e3dbef684b9
Jaren Garman <jarengarman@protonmail.com>
2025-01-01 18:36:46 -0500

    feat: Complete decrypt runner

--- enigma/lib/decrypt.rb
+++ enigma/lib/decrypt.rb
@@ -1,3 +1,9 @@
 # frozen_string_literal: true
 
 require_relative 'enigma'
+
+encrypted_file, filename, key, date = ARGV
+encrypted = File.read(encrypted_file)
+decrypted = Enigma.new.decrypt(encrypted, key, date)
+File.write(filename, decrypted[:decryption])
+puts "Created '#{filename}' with the key #{decrypted[:key]} and date #{decrypted[:date]}"
