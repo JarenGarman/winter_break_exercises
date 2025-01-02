97a16621e265bd09c0d7e9a5ecdd4fe04f8663ff
Jaren Garman <jarengarman@protonmail.com>
2025-01-01 21:52:25 -0500

    feat: Add filter_keys methods

--- enigma/lib/enigma.rb
+++ enigma/lib/enigma.rb
@@ -3,7 +3,7 @@
 require 'date'
 
 # TLD
-class Enigma
+class Enigma # rubocop:disable Metrics/ClassLength
   def encrypt(message, key = Random.new.rand(99_999).to_s.rjust(5, '0'), date = Date.today.strftime('%d%m%y'))
     {
       encryption: get_output(message, key, date, 1),
@@ -66,39 +66,67 @@ class Enigma
     end
   end
 
-  def get_key(message, date)
+  def get_key(message, date) # rubocop:disable Metrics/AbcSize
     key_offsets = get_key_offset(message[-4..].chars.each_with_index.map do |char, i|
       get_shift(char, i)
     end, (date.to_i**2).digits)
-    possible_keys = get_possible_keys(key_offsets)
+    possible_keys = filter_keys(get_possible_keys(key_offsets))
+    possible_keys = filter_keys(possible_keys) until possible_keys.map(&:length).all?(1) || possible_keys.any?([])
+    possible_keys[0][0] + possible_keys[2][0] + possible_keys[3][0][1]
   end
 
   def get_shift(char, index)
     char_i = char_set.find_index(char)
     index_i = char_set.find_index(crack_ending[index])
-    if char_i > index_i
+    if char_i >= index_i
       char_i - index_i
     else
-      index_i - char_i
+      27 - (index_i - char_i)
     end
   end
 
   def get_key_offset(shifts, date)
     shifts.each_with_index.map do |shift, i|
-      shift - date[3 - i]
+      offset = shift - date[3 - i]
+      offset += 27 if offset.negative?
+      offset
     end
   end
 
   def get_possible_keys(key_offsets)
-    key_offsets.group_by do |key_offset|
+    key_offsets.map do |key_offset|
       array = [key_offset]
-      until key_offset > 99
+      until key_offset + 27 > 99
         key_offset += 27
         array << key_offset
       end
-      array.map do |possible_num|
-        possible_num.to_s.rjust(2, '0')
+      array.map { |possible_num| possible_num.to_s.rjust(2, '0') }
+    end
+  end
+
+  def filter_keys(possible_keys)
+    filtered_keys = []
+    possible_keys.each_cons(2) do |first_keys, second_keys|
+      filtered_keys << first_keys.select do |first_key|
+        second_keys.map do |second_key|
+          second_key[0]
+        end.any?(first_key[1])
       end
-    end.invert
+    end
+    filtered_keys << possible_keys.last
+    filter_keys_backwards(filtered_keys)
+  end
+
+  def filter_keys_backwards(possible_keys)
+    filtered_keys = []
+    possible_keys.reverse.each_cons(2) do |first_keys, second_keys|
+      filtered_keys << first_keys.select do |first_key|
+        second_keys.map do |second_key|
+          second_key[1]
+        end.any?(first_key[0])
+      end
+    end
+    filtered_keys << possible_keys.first
+    filtered_keys.reverse
   end
 end
